f4fe5e41535ea08a800f7e85a7f7fdfca8447467
Aman Ullah Akhand <87015561+AmanUllahAkhand@users.noreply.github.com>
2026-01-17 21:44:05 +0600

    feat(splash): add centered "Pray App" text to splash screen

--- lib/presentation/views/splash_screen.dart
+++ lib/presentation/views/splash_screen.dart
@@ -6,6 +6,7 @@ import 'package:get/get.dart';
 import 'package:pray_app/core/constants/app_colors.dart';
 import 'package:pray_app/core/constants/app_icons.dart';
 import 'package:pray_app/core/routes/app_routes.dart';
+import 'package:pray_app/presentation/widgets/custom_text.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -25,8 +26,25 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primaryColor,
-      body: Center(
-        child: SvgPicture.asset(AppIcons.splashBackground),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          // Background SVG
+          SvgPicture.asset(
+            AppIcons.splashBackground,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+
+          // App Name Text
+          const CustomText(
+            text: 'Pray App',
+            fontSize: 28,
+            fontWeight: FontWeight.w600,
+            color: backgroundColor,
+          ),
+        ],
       ),
     );
   }
